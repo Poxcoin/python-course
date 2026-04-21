97048d69ed6189375c9be2cf32fec04de0502b34
davud <dhebebd8@gmail.com>
2026-04-21 17:47:14 -0400

    feat: calculate profit

--- start.py
+++ start.py
@@ -1,45 +1,24 @@
-from rich.console import Console
-from rich.table import Table
-
-history = []
-total_balance = 0  
-console = Console()
-while True:
-    coin_name = input("Введите имя монети: ")
-
-    if coin_name == "exit":
-        break
-
-    buy_price = float(input("write price: "))
-    sell_price = float(input("write sell price: "))
-    amount = float(input("how much buy: "))
-
-    profit = (sell_price - buy_price) * amount
-
-    print(f"я слежу за монетой{coin_name}")
-    print(f"Buy price {buy_price}")
-
-    if profit > 0:
-        print(f"china: {profit}")
-    elif profit < 0:
-        print(f"fck: {profit}")
+def format_result(coin_name:str, profit:float) -> str:
+    if profit < 0:
+        return f"{coin_name}: -{abs(profit)} USDT"
+    elif profit > 0:
+        return f"{coin_name}: +{abs(profit)} USDT"
     else:
-        print(f"zero")
-    
-    trade_info = {"name": coin_name, "result": profit}
-    
-    history.append(trade_info)
-    total_balance = total_balance + profit
-    
-    table = Table(title="Trade History")
-    table.add_column("Coin")
-    table.add_column("Profit")
-    for trade in history:
-        name = trade["name"]
-        profit = trade ["result"]
-        table.add_row(name, f"{profit}USDT")
-        
-    console.print(table)
-    console.print(f"Total balance: {total_balance} USDT", style="bold blue")
+        return f"{coin_name}: {profit} USDT"
 
-    print("\n----------------")
+def calculate_profit(buy_price:float, sell_price:float, amount:float) -> float:
+    return (sell_price - buy_price) * amount
+
+def run_tracker() -> None:
+    while True:
+        coin_name = input(f"Enter the name of the coin: ")
+        if coin_name == "exit":
+            break
+        buy_price = float(input(f"Enter the buy price of {coin_name}: "))
+        sell_price = float(input(f"Enter the sell price of {coin_name}: "))
+        amount = float(input(f"Enter the amount of {coin_name} to trade: "))
+        profit = calculate_profit(buy_price, sell_price, amount)
+        print(format_result(coin_name, profit))
+        
+if __name__ == "__main__":    
+    run_tracker()
